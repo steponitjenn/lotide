c1afabda23ba4e8042de3850dcc73114d9ff9b93
Jenn <jfreeze80@gmail.com>
2022-05-25 22:07:57 -0600

    made assertion funtion

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✔ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("froot", "loops");
+assertEqual("candy", "candy");
+assertEqual(12, 16);
