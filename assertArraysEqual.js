0ec0b370663accc8ce307ebe434a70bf5846ef1d
Jenn <jfreeze80@gmail.com>
2022-06-01 22:27:23 -0600

    check if arrays match

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(arr1, arr2) {
+  for (let i = 0; i <= arr1.length || i <= arr2.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log("❌ Assertion Failed: Arrays do not match")
+      return false
+  } else {
+    
+  } 
+  } 
+  console.log("✔ Assertion Passed: Arrays match")
+  return true
+};
+
+
+
+
+
+(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+
+(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); // => false
+(assertArraysEqual([1], ["1"]), false);
+
+
+
+
+
+
