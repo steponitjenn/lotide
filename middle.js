dfac3cf8e2134ba96851ca92e9d041a522c11686
Jenn <jfreeze80@gmail.com>
2022-06-26 22:54:36 -0600

    refactor middle and add middle test

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-// const assertArraysEqual = function(arr1, arr2) {
-//   for (let i = 0; i <= arr1.length || i <= arr2.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       console.log("❌ Assertion Failed: Arrays do not match")
-//       return false
-//   } else {
-//   }
-//   }
-//   console.log("✔ Assertion Passed: Arrays match")
-//   return true
-// };
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`:) Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`:() Assertion Failed: ${actual} !== ${expected}`);
-//   }
-
-// };
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = (array) => {
   if (array.length <= 2) {
@@ -32,30 +13,4 @@ const middle = (array) => {
   }
 };
 
-
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, "lemon", 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 33, 44, 5, 6])) // => [3, 4]
-
-
-// // TEST/ASSERTION FUNCTIONS
-// const eqArrays = function(...) {
-//   //...
-// }
-
-// const assertArraysEqual = function(actual, expected) {
-//   //...
-// }
-
-// // ACTUAL FUNCTION
-// const middle = function(array) {
-//   //...
-// }
-
-// // TEST CODE
-// // ...
+module.exports = middle;
